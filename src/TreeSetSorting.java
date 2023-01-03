23e32335945add55c52a824cac3059c9c10eacf9
Rimpal-Patel <rimpal@7span.com>
2023-01-03 11:05:54 +0530

    Initial commit

---
+++ src/TreeSetSorting.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class HashSetSorting
+{
+	public static void main(String[] args)
+	{
+		Set myHashSet= new TreeSet();
+	}
+}
