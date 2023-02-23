2a0c9c5777c24953365508e1b4884040a1e5007d
Rimpal-7span <rimpal@7span.com>
2023-02-23 17:48:16 +0530

    Executor service program

--- src/executor_service/ExecutorServiceDemo.java
+++ src/executor_service/ExecutorServiceDemo.java
@@ -7,18 +7,59 @@ public class ExecutorServiceDemo
 {
 	public static void main(String[] args)
 	{
-		Runnable runnable = ()->
+		ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+			executorService.execute(new ThreadCreation());
+			executorService.execute(new ThreadCreation1());
+
+		for(int i=1;i<=5;i++)
 		{
-			for(int i=1;i<=5;i++)
-			{
-				System.out.println("i is =>"+i);
-			}
-		};
-		ExecutorService executorService = Executors.newFixedThreadPool(2);
-		executorService.execute(runnable);
-		executorService.execute(runnable);
-		executorService.execute(runnable);
-		executorService.execute(runnable);
-		executorService.execute(runnable);
+			System.out.println("main is =>"+i+"with current thread"+ Thread.currentThread().getName());
+		}
+
 	}
+
+
+	}
+
+class ThreadCreation extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("i is =>"+i+"with current thread"+ currentThread().getName());
+		}
+	}
+
+}
+
+class ThreadCreation1 extends Thread
+{
+	public void run()
+	{
+		for(int k=1;k<=5;k++)
+		{
+			System.out.println("k is =>"+k+"with current thread"+ currentThread().getName());
+		}
+	}
+
+}
+
+class ThreadCreation2 extends Thread
+{
+	int count=0;
+	public void run()
+	{
+		for(int j=1;j<=5;j++)
+		{
+			if(j==3)
+				for(int l=1;l<=50000;l++)
+				{
+						count=count+1;
+				}
+			System.out.println("j is =>"+j+"with current thread"+ currentThread().getName());
+		}
+	}
+
 }
